ca770c128a1207e2b2165aa85bc12b4c69ce627a
Pascal Immerzeel <pascal@immerzeelpictures.com>
2014-05-08 00:29:56 +0200

    Initial commit

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'immerzeel-jekyll'
+version '0.1.0'
+source 'git://git@github.com/immerzeel:puppet-jekyll.git'
+author 'Pascal Immerzeel'
+license 'Apache License, Version 2.0'
+summary 'Jekyll Module'
+description 'Install and configure Jekyll'
+project_page 'http://github.com/immerzeel/puppet-jekyll'
+
+## Add dependencies, if any:
+# dependency 'username/name', '>= 1.2.0'
---
+++ manifests/init.pp
@@ -0,0 +1,26 @@
+# == Class: jekyll
+#
+# Install jekyll for static website creation.
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+# include jekyll
+#
+# === Authors
+#
+# Pascal Immerzeel <pascal@immerzeel.net>
+#
+# === Copyright
+#
+# Copyright 2014 Pascal Immerzeel, unless otherwise noted.
+#
+class jekyll {
+    package {['jekyll', 'sass']:
+        ensure => installed,
+        provider => gem
+    }
+}
